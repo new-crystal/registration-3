a5188fe7a4a024950b9ed46a41034d4796d1fa46
new-crystal <oj3339@naver.com>
2023-11-14 09:12:50 +0900

    edit email subject

--- application/views/row_scan_qr.php
+++ application/views/row_scan_qr.php
@@ -6,107 +6,119 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <style>
-    body {
-        font-family: 'Roboto', sans-serif;
+@keyframes fadeInUp {
+    0% {
+        opacity: 0;
+        transform: translate3d(0, 100%, 0);
     }
 
-    input #accessForm {
-        padding: 0 3rem;
-        /* height: 60%; */
+    to {
+        opacity: 1;
+        transform: translateZ(0);
     }
+}
 
-    #qrcode:focus {
-        outline: none;
-    }
 
-    .qr_info_wrap {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        height: 5.5rem;
-        /* border: 1px solid #eee; */
-        margin: 1rem auto;
-        font-weight: 500;
-        font-size: 2.5rem;
-    }
+body {
+    font-family: 'Roboto', sans-serif;
+}
 
-    .info_name {
-        width: 33%;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: rgb(49 46 129);
-        color: white;
+input #accessForm {
+    padding: 0 3rem;
+    /* height: 60%; */
+}
 
-    }
+#qrcode:focus {
+    outline: none;
+}
 
-    .info_content {
-        width: 66%;
-        height: 100%;
-        /* border: 2px solid rgb(49 46 129); */
-    }
+.qr_info_wrap {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 5.5rem;
+    /* border: 1px solid #eee; */
+    margin: 1rem auto;
+    font-weight: 500;
+    font-size: 2.5rem;
+}
 
-    .info_content>input {
-        margin-left: 139px;
-        margin-top: 10px;
-        width: 141%;
-        height: 90%;
-        padding: 0 2rem;
-        z-index: 999;
-        transform: translate(536px, -166px);
-    }
+.info_name {
+    width: 33%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(49 46 129);
+    color: white;
 
-    input {
-        /* background-color: orange; */
-        background-color: transparent;
-        font-size: 3rem;
-        font-family: 'Roboto', sans-serif !important;
-    }
+}
 
-    .info_content>input:focus {
-        outline: none
-    }
+.info_content {
+    width: 66%;
+    height: 100%;
+    /* border: 2px solid rgb(49 46 129); */
+}
 
-    #text_box {
-        font-size: 1.88rem;
-    }
+.info_content>input {
+    margin-left: 139px;
+    margin-top: 10px;
+    width: 157%;
+    height: 90%;
+    padding: 0 2rem;
+    z-index: 999;
+    transform: translate(-63px, 91px);
+}
 
-    .fresh {
-        width: 152%;
-        height: 291px;
-        /* background-color: #ddd; */
-        transform: translate(71px, 114px);
-    }
+input {
+    /* background-color: orange; */
+    background-color: transparent;
+    font-size: 3rem;
+    font-family: 'Roboto', sans-serif !important;
+}
 
-    .input_box {
-        transform: translate(1px, -92px);
-    }
+.info_content>input:focus {
+    outline: none
+}
 
-    .alert {
-        width: 500px;
-        height: 200px;
-        background: rgb(151, 197, 239);
-        background: linear-gradient(125deg, rgba(151, 197, 239, 1) 0%, rgba(214, 235, 255, 1) 50%, rgba(214, 235, 255, 1) 100%);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: #003366;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        border-radius: 32px;
-    }
+#text_box {
+    font-size: 1.88rem;
+}
 
-    .alert>p {
-        font-size: 3rem;
-        font-weight: 700;
-        position: relative;
-        animation: fadeInUp 1s;
-    }
+.fresh {
+    width: 152%;
+    height: 291px;
+    /* background-color: #ddd; */
+    transform: translate(71px, 114px);
+}
+
+.input_box {
+    transform: translate(1px, -92px);
+}
+
+.alert {
+    width: 500px;
+    height: 200px;
+    background: #7c035f;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFF;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 32px;
+}
+
+.alert>p {
+    font-size: 3rem;
+    font-weight: 700;
+    position: relative;
+    animation: fadeInUp 1s;
+}
 </style>
 
 <body id="body" class="flex items-center justify-center">
@@ -117,23 +129,24 @@
         <div class="h-full">
             <div>
                 <div>
-                    <img src="../../assets/images/row_app_loading_bg.png" onclick="window.location.replace()" style="position: absolute;z-index: -999;width: 1920px;" />
+                    <img src="../../assets/images/row_app_loading_bg.png" onclick="window.location.replace()"
+                        style="position: absolute;z-index: -999;width: 1920px;" />
                     <dl>
 
                         <script type="text/javascript">
-                            $(function() {
-                                $("#accessForm").submit(function() {
-                                    if (!$.trim($("#qrcode").val())) {
-                                        alert("QR CODE를 입력하세요.");
-                                        $("#qrcode").focus();
-                                        return false;
-                                    }
+                        $(function() {
+                            $("#accessForm").submit(function() {
+                                if (!$.trim($("#qrcode").val())) {
+                                    alert("QR CODE를 입력하세요.");
+                                    $("#qrcode").focus();
+                                    return false;
+                                }
 
-                                    $("#accessForm").attr("action", "/access/row_scan_qr");
+                                $("#accessForm").attr("action", "/access/row_scan_qr");
 
-                                    return true;
-                                });
+                                return true;
                             });
+                        });
                         </script>
                         <div class="input_box">
                             <?php echo form_open('/access/row_scan_qr', 'id="accessForm" name="accessForm"') ?>
@@ -141,13 +154,18 @@
                                 <div class="fresh"></div>
                                 <div style=" transform: translateY(700px);">
                                     <dl class="pl-2" style="transform: translateY(-200px);">
-                                        <dd><input type="text" name="qrcode" id="qrcode" class=" h-20  px-3 py-3 mt-5 border-indigo-900 mx-auto" style="transform:translate(105px,-61px);width: 800px;" placeholder="" autofocus>
+                                        <dd><input type="text" name="qrcode" id="qrcode"
+                                                class=" h-20  px-3 py-3 mt-5 border-indigo-900 mx-auto"
+                                                style="    transform: translate(74px,-20px);width: 800px;"
+                                                placeholder="" autofocus>
                                         </dd>
                                     </dl>
 
                                     <dl class="pl-2" style="transform: translateY(-200px);">
                                         <div id="qr_nick_name" class="qr_info_wrap">
-                                            <div class="info_content"><input type="text" class="qr_info input name" value="<?php if (isset($first_name)) echo $first_name . ' ' . $last_name ?>" readonly>
+                                            <div class="info_content"><input type="text" class="qr_info input name"
+                                                    value="<?php if (isset($first_name)) echo $first_name . ' ' . $last_name ?>"
+                                                    readonly>
                                             </div>
                                         </div>
 
@@ -156,7 +174,9 @@
                                     <dl class="pl-2" style="transform: translateY(-200px);">
                                         <div id="qr_entrance" class="qr_info_wrap">
                                             <div class="info_content">
-                                                <input type="text" style="margin-top:3px;transform: translate(-327px, -66px);" class="qr_info input" value="<?php if (isset($enter)) {
+                                                <input type="text"
+                                                    style="margin-top:3px;transform: translate(857px, -226px);"
+                                                    class="qr_info input" value="<?php if (isset($enter)) {
                                                                                                                                                                 $enter = date("Y-m-d H:i", strtotime($enter));
                                                                                                                                                                 echo $enter;
                                                                                                                                                             } ?>
@@ -166,38 +186,42 @@
                                         </div>
                                         <div id="qr_exit" class="qr_info_wrap">
                                             <div class="info_content">
-                                                <input type="text" style="margin-top:0;    transform: translate(534px, -166px);" class="qr_info input" value="<?php if (isset($leave)) {
-                                                                                                                                                                    $leave = date("Y-m-d H:i", strtotime($leave));
-                                                                                                                                                                    echo $leave;
-                                                                                                                                                                } ?>
+                                                <input type="text"
+                                                    style="margin-top:0; transform: translate(854px, -107px);"
+                                                    class="qr_info input" value="<?php if (isset($leave)) {
+                                                                                                                                                                $leave = date("Y-m-d H:i", strtotime($leave));
+                                                                                                                                                                echo $leave;
+                                                                                                                                                            } ?>
                                                 " readonly>
                                             </div>
                                         </div>
                                     </dl>
 
                                     <div class="w-full flex items-center justify-center">
-                                        <button type="submit" value="등록" class="btnPoint w-full flex items-center justify-center" style="    transform: translate(55px,434px);"></button>
+                                        <button type="submit" value="등록"
+                                            class="btnPoint w-full flex items-center justify-center"
+                                            style="    transform: translate(55px,434px);"></button>
                                     </div>
                                 </div>
 
                             </fieldset>
                             </form>
                             <script type="text/javascript">
-                                window.scrollTo(0, document.body.scrollHeight);
-                                $("#qrcode").focus();
-                                $(document).ready(function() {
-                                    setTimeout(function() {
-                                        $('.qr_info input').val('');
-                                        $('.qr_txt').hide();
-                                        $("#qrcode").focus();
-                                    }, 10000);
-                                })
-                                const qrcode = document.querySelector("#qrcode");
-                                const accessForm = document.querySelector("#accessForm")
-                                accessForm.addEventListener("submit", (e) => {
-                                    // e.preventDefault();
-                                    qrcdoe.valuea.replace(/ /g, "")
-                                })
+                            window.scrollTo(0, document.body.scrollHeight);
+                            $("#qrcode").focus();
+                            $(document).ready(function() {
+                                setTimeout(function() {
+                                    $('.qr_info input').val('');
+                                    $('.qr_txt').hide();
+                                    $("#qrcode").focus();
+                                }, 10000);
+                            })
+                            const qrcode = document.querySelector("#qrcode");
+                            const accessForm = document.querySelector("#accessForm")
+                            accessForm.addEventListener("submit", (e) => {
+                                // e.preventDefault();
+                                qrcdoe.valuea.replace(/ /g, "")
+                            })
                             </script>
                         </div>
                     </dl>
@@ -206,59 +230,59 @@
 
 </body>
 <script>
-    const inputs = document.querySelectorAll(".qr_info");
-    const qrcodeInput = document.querySelector("#qrcode");
-    const freshBtn = document.querySelector(".fresh")
-    const body = document.querySelector("#body")
-    const alert = document.querySelector(".alert")
-    const alertText = document.querySelector(".alert_text")
-    const name = document.querySelector(".name")
-    let textTime;
-    let alertTime;
+const inputs = document.querySelectorAll(".qr_info");
+const qrcodeInput = document.querySelector("#qrcode");
+const freshBtn = document.querySelector(".fresh")
+const body = document.querySelector("#body")
+const alert = document.querySelector(".alert")
+const alertText = document.querySelector(".alert_text")
+const name = document.querySelector(".name")
+let textTime;
+let alertTime;
 
 
-    body.addEventListener("click", () => {
-        qrcodeInput.focus()
-    })
+body.addEventListener("click", () => {
+    qrcodeInput.focus()
+})
 
-    function checkAlert() {
-        if (name.value !== "") {
-            alert.style.display = "";
-        } else {
-            alert.style.display = "none";
-        }
+function checkAlert() {
+    if (name.value !== "") {
+        alert.style.display = "";
+    } else {
+        alert.style.display = "none";
     }
+}
 
 
-    freshBtn.addEventListener("touchstart", () => {
-        window.location.reload()
-    })
+freshBtn.addEventListener("touchstart", () => {
+    window.location.reload()
+})
 
-    qrcodeInput.addEventListener("input", (e) => {
-        // 입력된 값에서 공백 제거
-        const newValue = e.target.value.replace(/\s+/g, "");
+qrcodeInput.addEventListener("input", (e) => {
+    // 입력된 값에서 공백 제거
+    const newValue = e.target.value.replace(/\s+/g, "");
 
-        // 입력된 값 업데이트
-        e.target.value = newValue;
-    })
+    // 입력된 값 업데이트
+    e.target.value = newValue;
+})
 
-    window.onload = () => {
-        qrcodeInput.focus()
-        clearTimeout(alertTime);
-        clearTimeout(textTime)
-        checkAlert()
-        alertTime = setTimeout(() => {
+window.onload = () => {
+    qrcodeInput.focus()
+    clearTimeout(alertTime);
+    clearTimeout(textTime)
+    checkAlert()
+    alertTime = setTimeout(() => {
+        alert.style.display = "none";
+    }, 3000)
+    inputs.forEach((input) => {
+        textTime = setTimeout(() => {
+            input.value = "";
             alert.style.display = "none";
-        }, 3000)
-        inputs.forEach((input) => {
-            textTime = setTimeout(() => {
-                input.value = "";
-                alert.style.display = "none";
-            }, 10000)
-        })
-    }
-    /**우클릭 방지 */
-    document.addEventListener("contextmenu", function(event) {
-        event.preventDefault();
-    }, false);
+        }, 10000)
+    })
+}
+/**우클릭 방지 */
+document.addEventListener("contextmenu", function(event) {
+    event.preventDefault();
+}, false);
 </script>
